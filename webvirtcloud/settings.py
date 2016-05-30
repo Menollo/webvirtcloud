32717d173b375e921f43a220f675c00441811ac9
Jacco Koning <jacco@vevida.nl>
2016-05-30 17:58:06 +0200

    remove RemoteUserBacken and fix LOGIN_URL

--- webvirtcloud/settings.py
+++ webvirtcloud/settings.py
@@ -38,18 +38,12 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.RemoteUserBackend',
-    #'accounts.backends.MyRemoteUserBackend',
-)
-
-LOGIN_URL = '/accounts/login'
+LOGIN_URL = '/accounts/login/'
 
 ROOT_URLCONF = 'webvirtcloud.urls'
 
